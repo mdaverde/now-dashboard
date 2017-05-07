3ca7f017093b1a0e9731f2976a20847cb3c06bd7
Marlon Landaverde <milanlandaverde@gmail.com>
2017-05-06 21:50:52 -0500

    Shows aliases

--- components/IndexPage/Deployments/components/DeploymentGroup/components/DeployItem/components/DeployRow/index.js
+++ components/IndexPage/Deployments/components/DeploymentGroup/components/DeployItem/components/DeployRow/index.js
@@ -1,4 +1,6 @@
+import { Component } from 'react';
 import moment from 'moment';
+import _ from 'lodash';
 import DeployState from './components/DeployState';
 
 function calendar(created) {
@@ -7,67 +9,82 @@ function calendar(created) {
   });
 }
 
-export default function({ deployment, deleteDeployment }) {
-  const { created, state, scale: { current, max }, uid, url, aliases } = deployment;
+export default function ({ deployment, deleteDeployment }) {
+  const { created, state, scale: { current, max }, uid, url, aliases = [] } = deployment;
   return (
     <div className="root">
       <style jsx>{`
-        .root {
-          display: flex;
-        }
-        .deploy-property {
-          width: 100px;
-          margin-right: 16px;
-        }
-        .uid, .url {
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
-        }
-        .uid {
-          color: gray;
-          max-width: 220px;
-        }
-        .url {
-          width: 220px;
-        }
-        .created {
-          width: 200px;
-        }
-        a {
-          color: white;
-          text-decoration: none;
-        }
-        a:hover {
-          color: #06b0d7;
-          text-decoration: underline;
-        }
-        .remove-link, .aliases-link, .more-link {
-          width: 60px;
-        }
-        .remove-link:hover, .aliases-link:hover, .more-link:hover {
-          cursor: pointer;
-          text-decoration: underline;
-        }
-      `}</style>
+      .root {
+        display: flex;
+        transition: all 0.2s ease;
+      }
+      .deploy-property {
+        width: 100px;
+        margin-right: 16px;
+      }
+      .uid, .url {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+      .uid {
+        color: gray;
+        max-width: 220px;
+      }
+      .url {
+        width: 220px;
+      }
+      .link {
+        display: block;
+        text-align: center;
+      }
+      .aliases {
+        margin-top: 4px;
+      }
+      .alias {
+        display: block;
+        text-align: center;
+        color: #9575cd;
+      }
+      .created {
+        width: 200px;
+      }
+      a {
+        color: white;
+        text-decoration: none;
+      }
+      a:hover {
+        color: #06b0d7;
+        text-decoration: underline;
+      }
+      .remove-link, .aliases-link, .more-link {
+        width: 60px;
+      }
+      .remove-link:hover, .aliases-link:hover, .more-link:hover {
+        cursor: pointer;
+        text-decoration: underline;
+      }
+    `}</style>
       <div className="deploy-property uid">{uid}</div>
       <div className="deploy-property url">
-        <a href={`//${url}`}>{url}</a>
+        <a className="link" href={`//${url}`}>{url}</a>
+        {
+          !!(aliases && aliases.length) && (
+            <div className="aliases">
+              {
+                _.map(aliases, (link) => (
+                  <a key={link.uid} className="alias" href={`//${link.alias}`}>{link.alias}</a>
+                ))
+              }
+            </div>
+          )
+        }
       </div>
       <div className="deploy-property created">{calendar(created)}</div>
       <div className="deploy-property scale">{current} / {max} instances</div>
       <div className="deploy-property state">
         <DeployState state={state} />
       </div>
-      {
-        (aliases && aliases.length) ? (
-            <div className="deploy-property aliases-link">
-              aliases
-            </div>
-          ) : (
-            <div className="deploy-property aliases-link" />
-          )
-      }
       <div
         className="deploy-property remove-link"
         onClick={() => deleteDeployment(uid)}
--- components/IndexPage/Deployments/components/DeploymentGroup/components/DeployItem/index.js
+++ components/IndexPage/Deployments/components/DeploymentGroup/components/DeployItem/index.js
@@ -1,15 +1,19 @@
+import React from 'react';
 import DeployRow from './components/DeployRow';
 
 export default function({ deployment, deleteDeployment }) {
   return (
     <div className="root">
       <style jsx>{`
-        .root {
-          font-size: 12px;
-          margin-bottom: 8px;
-        }
-      `}</style>
-      <DeployRow deployment={deployment} deleteDeployment={deleteDeployment} />
+          .root {
+            font-size: 12px;
+            margin-bottom: 8px;
+          }
+        `}</style>
+      <DeployRow
+        deployment={deployment}
+        deleteDeployment={deleteDeployment}
+      />
     </div>
   );
 }
